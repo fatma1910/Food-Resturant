f06633fcd2ec4fadb40264a6fb493ff48e12df3d
unknown <fatmasaleh567@gmail.com>
2024-08-12 00:14:27 +0300

    update somethings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+    appDir: true, // Ensure this is enabled if using the new App directory structure
+  },
+};
 
 export default nextConfig;
